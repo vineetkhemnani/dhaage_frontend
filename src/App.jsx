451a1b5a5b44fc3148a828f00a5342264a34d5a4
vineetkhemnani <vineetkhemnani09@gmail.com>
2023-12-15 21:15:22 +0530

    trying routes

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
       <Header/>
       <Routes>
         <Route path="/:username" element={<UserPage/>}/>
+        <Route path="/user" element={<UserPage/>}/>
         <Route path="/:username/post/:pid" element={<PostPage/>}/>
       </Routes>
     </Container>
